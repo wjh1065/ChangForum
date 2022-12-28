06996f117188467b10fc68dd19f925e03a15d556
wjh1065 <wjh1065@naver.com>
2022-12-28 13:26:25 +0900

    feat

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,6 +1,6 @@
 from .base import *
 
-ALLOWED_HOSTS = ['3.34.219.164']
+ALLOWED_HOSTS = ['3.34.219.164', 'changseok.com']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 DEBUG = False
--- pybo/views/base_views.py
+++ pybo/views/base_views.py
@@ -4,7 +4,11 @@ from django.shortcuts import render, get_object_or_404
 from ..models import Question
 from django.db.models import Q
 
+import logging
+logger = logging.getLogger('pybo')
+
 def index(request):
+    logger.info("INFO 레벨로 출력")
     page = request.GET.get('page', '1')
     kw = request.GET.get('kw', '')  # 검색어
     question_list = Question.objects.order_by('-create_date')
