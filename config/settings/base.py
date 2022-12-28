e025a88f5c76dbe9c8f04b99e30f0ab00faca7bc
wjh1065 <wjh1065@naver.com>
2022-12-28 12:46:03 +0900

    feat

--- config/settings/base.py
+++ config/settings/base.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&7=@jv+m#8jtcd4797vs!)c)k*mp7*5&72pk*uct6l$2opb+lr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['3.34.219.164']
 
 
 # Application definition
---
+++ config/settings/prod.py
@@ -0,0 +1,6 @@
+from .base import *
+
+ALLOWED_HOSTS = ['3.34.219.164']
+STATIC_ROOT = BASE_DIR / 'static/'
+STATICFILES_DIRS = []
+DEBUG = False
