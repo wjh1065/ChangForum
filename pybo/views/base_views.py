48b31f50f8260565f0abfb2f203bbd6f875a9aa3
wjh1065 <wjh1065@naver.com>
2022-12-28 14:28:26 +0900

    final init

--- pybo/views/base_views.py
+++ pybo/views/base_views.py
@@ -8,7 +8,7 @@ import logging
 logger = logging.getLogger('pybo')
 
 def index(request):
-    logger.info("INFO 레벨로 출력")
+    # logger.info("INFO 레벨로 출력")
     page = request.GET.get('page', '1')
     kw = request.GET.get('kw', '')  # 검색어
     question_list = Question.objects.order_by('-create_date')
